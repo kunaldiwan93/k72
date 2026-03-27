a05d4f5fd2e7b74e6eef5a9a02b0a76481036355
kunaldiwan93 <kunal39diwan@gmail.com>
2026-03-27 21:53:10 +0530

    import fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import FullScreenNav from './components/Navigation/FullScreenNav'
-import Navbar from './components/Navigation/navbar'
-import Agence from './pages/agence'
+import Navbar from './components/Navigation/Navbar'
+import Agence from './pages/Agence'
 
 
 const App = () => {
